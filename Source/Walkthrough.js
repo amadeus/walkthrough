f2b96d51b7f3130e27216ed8196788906cc2b847
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2011-11-04 16:44:16 -0700

    Implemented feedback and bug fixes from code review
    * Fixed hyphen bug
    * Account for invalid elements in addViews
    * Allow multiple close buttons
    * Implement dispose in view class
    * Fixed public methods to show and hide
    * Made displayView private
    * Added return this to all public methods
    * Renamed jackLinks to linkHandler

--- Source/Walkthrough.js
+++ Source/Walkthrough.js
@@ -21,78 +21,89 @@ var Walkthrough = this.Walkthrough = new Class({
 	Implements: [Options, Events, Class.Binds],
 
 	options: {
-		prefix: 'walkthrough-',
+		prefix: 'walkthrough',
 		urlPrefix: 'walkthrough',
-		startsWith: 'intro',
-		currentView: null,
-		storeState: true
+		startsWith: 'intro'
 	},
 
 	views: {},
-	current: {},
+	currentView: {},
 
 	initialize: function(viewer, content, options){
 		this.viewer = document.id(viewer);
 		this.setOptions(options);
-		if (!this.viewer) return dbg.log('Walkthrough: Missing something...', this.viewer);
+		if (!this.viewer) return;
 
 		if (content) {
 			content = document.id(content).dispose();
 			this.addViews(content);
 		}
 
-		var state = window.localStorage[this.options.prefix + '-state'];
-		if (state) this.options.currentView = state;
-
-		this.displayView();
+		this.show();
 	},
 
-	displayView: function(view){
-		if (!view) view = this.options.currentView || this.options.startsWith;
+	show: function(view){
+		// If no view specified and there's a current view inject it, otherwise use startsWith
+		if (!view && this.currentView.name) {
+			this.currentView.inject(this.viewer);
+			return this.fireEvent('show', this.currentView.name);
+		}
+		else if (!view) view = this.options.startsWith;
+
+		this._displayView(view);
+		return this;
+	},
+
+	hide: function(){
+		if (this.currentView.name) {
+			this.currentView.dispose();
+			this.fireEvent('hide', this.currentView.name);
+		}
+		return this;
+	},
+
+	addViews: function(els){
+		var mapEl = function(el){
+			el = document.id(el);
+			if (typeOf(el) !== 'element') return;
+			el.getElements('script').destroy();
+			this.views[el.get('data-' + this.options.prefix + '-slide-id')] = el;
+		};
+
+		if (typeOf(els) === 'element') els = els.getChildren();
+
+		new Elements(els).each(mapEl, this);
+		return this;
+	},
+
+	clean: function(){
+		this._cleanView();
+		return this;
+	},
+
+	_displayView: function(view){
+		// Validate that we have the view
 		if (!this.views[view]) return this.fireEvent('error', {
 			type: 'navigation',
 			message: 'This page does not exist',
 			view: view
 		});
 
-		this.options.currentView = view;
 		this._cleanView();
 
-		this.current = new View(view, this.views[view], {
+		this.currentView = new View(view, this.views[view], {
 			prefix: this.options.prefix,
 			urlPrefix: this.options.urlPrefix,
-			onNavigation: this.bound('displayView'),
+			onNavigation: this.bound('_displayView'),
 			onClose: this.bound('hide')
-		});
+		}).inject(this.viewer);
 
-		this.current.inject(this.viewer);
-
-		window.localStorage[this.options.prefix + '-state'] = view;
-	},
-
-	addViews: function(els){
-		var mapEl = function(el){
-			el = document.id(el);
-			this.views[el.get('data-' + this.options.prefix + 'id')] = el;
-		};
-
-		if (typeOf(els) === 'element') els = els.getChildren();
-
-		new Elements(els).each(mapEl, this);
-	},
-
-
-	hide: function(clean){
-		if (clean === true) {
-			delete window.localStorage[this.options.prefix + '-state'];
-			delete this.options.currentView;
-		}
-		this._cleanView();
+		this.fireEvent('navigation', this.currentView.name);
 	},
 
 	_cleanView: function(){
 		// Clean existing classes and elements
-		if (this.current.clean) this.current.clean();
+		if (this.currentView.clean) this.currentView.clean();
 
 		// I realize this is probably over the top, but just in case...
 		this.viewer.empty();
@@ -115,13 +126,18 @@ var View = Walkthrough.View = new Class({
 		this.name = name;
 		this.el = document.id(el).clone(true, true);
 		this.setOptions(options);
-		this.el.addEvent('click:relay(a)', this.bound('_jackLinks'));
-		this.closeButton = this.el.getElement('.' + this.options.prefix + 'close');
-		if (this.closeButton) this.closeButton.addEvent('click', this.bound('_closeView'));
+		this.el.addEvent('click:relay(a)', this.bound('_linkHandler'));
+		this.el.addEvent('click:relay([data-' + this.options.prefix + '-close])', this.bound('_closeView'));
 	},
 
 	inject: function(container){
 		this.el.inject(container);
+		return this;
+	},
+
+	dispose: function(){
+		this.el.dispose();
+		return this;
 	},
 
 	clean: function(){
@@ -131,9 +147,11 @@ var View = Walkthrough.View = new Class({
 
 		if (this.el) this.el.destroy();
 
+		delete this.name;
 		delete this.el;
 		delete this.classes;
 		delete this.clean;
+		return this;
 	},
 
 	_closeView: function(e){
@@ -141,7 +159,7 @@ var View = Walkthrough.View = new Class({
 		this.fireEvent('close');
 	},
 
-	_jackLinks: function(e, el){
+	_linkHandler: function(e, el){
 		var paths = Walkthrough.View.getPaths(el.get('href'));
 		if (paths[0] === this.options.urlPrefix) {
 			e.preventDefault();
